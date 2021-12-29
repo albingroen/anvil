37c50f3c2a4f8f194ca4a6a1d1e6b187ffec7c50
Albin Groen <albin.groen@gmail.com>
2021-12-29 21:54:09 +0100

    Take in project name argument

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 import clientSetup from './client/setup'
 import serverSetup from './server/setup'
+import fs from 'fs'
 
 function main() {
+  const projectName = process.argv[2]
+
+  if (projectName) {
+    fs.mkdirSync(projectName)
+    process.chdir(projectName)
+  }
+
   clientSetup()
   process.chdir('../')
   serverSetup()
